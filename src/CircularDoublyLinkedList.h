dfa6bc8fb90b4d067001ceb88a514d2530802d2a
noskovm <maks-noskov-02-02@mail.ru>
2023-11-13 00:08:09 +0300

    feat/Добавление логики удаления и логики поведения методов в зависимости от направления

--- src/CircularDoublyLinkedList.h
+++ src/CircularDoublyLinkedList.h
@@ -5,11 +5,15 @@
 template<typename T>
 class Node {
 public:
-    Node(T p_data = T(), Node<T>* p_prev = nullptr, Node<T>* p_next = nullptr):
+    //Узел будет хранить Индекс(Обязательный параметр) и Данные(если не зададут, то будет вызван конструктор по умолчанию)
+    Node(int p_index, T p_data = T(), Node<T>* p_prev = nullptr, Node<T>* p_next = nullptr):
+    index(p_index),
     data(p_data),
     prev(p_prev),
     next(p_next) {}
 
+
+    int index;
     T data;
     Node<T>* prev;
     Node<T>* next;
@@ -26,16 +30,22 @@ public:
         CLOCKWISE,
         COUNTERCLOCKWISE
     };
-    void set_direction(Direction dir);
+
+    void set_clockwise_direction();
+    void set_counterclockwise_direction();
     Direction get_direction();
 
     void push_next(T data);
+    int pull();
     void go_next();
     Node<T> get_node();
+    int len();
 
 private:
     Direction dir;
+    Node<T>* head;
     Node<T>* current;
+    int length;
 };
 
 
@@ -45,12 +55,19 @@ private:
 template<typename T>
 CircularDoublyLinkedList<T>::CircularDoublyLinkedList():
     current(nullptr),
-    dir(CircularDoublyLinkedList::Direction::CLOCKWISE) {}
+    dir(CircularDoublyLinkedList::Direction::CLOCKWISE),
+    length(0) {}
 
-// set_direction принимает на вход enum Direction и задает направление обхода кольцевого списка
+// set_clockwise_direction задает направление обхода списка по часовой стрелке
 template<typename T>
-void CircularDoublyLinkedList<T>::set_direction (Direction dir){
-    this->dir = dir;
+void CircularDoublyLinkedList<T>::set_clockwise_direction(){
+    this->dir = Direction::CLOCKWISE;
+}
+
+// set_counterclockwise_direction задает направление обхода списка против часовой стрелки
+template<typename T>
+void CircularDoublyLinkedList<T>::set_counterclockwise_direction(){
+    this->dir = Direction::COUNTERCLOCKWISE;
 }
 
 // get_direction возвращает направление обхода кольцевого списка
@@ -60,30 +77,66 @@ typename CircularDoublyLinkedList<T>::Direction CircularDoublyLinkedList<T>::get
 }
 
 
-// push_next добавляет новый узел списка после текущего узла и устанавливает текущий на место вновь прибывшего
+// push_next добавляет новый узел списка после текущего узла и устанавливает current на место вновь прибывшего
 template<typename T>
 void CircularDoublyLinkedList<T>::push_next(T data) {
-    Node<T>* new_node = new Node<T>(data);
+    Node<T>* newNode = new Node<T>(len(), data); // index = текущая длина, удобно, когда нумерация с 0
 
     // если пустой, то current теперь одна нода, зацикленная на себя
     if (!current) {
-        current = new_node;
-        new_node->next = current;
-        new_node->prev = current;
-    } else {
-        new_node->prev = current;
-        (current->next)->prev = new_node;
-        new_node->next = current->next;
-        current->next = new_node;
-        current = new_node;
+        current = newNode;
+        newNode->next = current;
+        newNode->prev = current;
+    } else { // если не пустой, то смотрим в каком направлении вставлять
+        if (dir == CircularDoublyLinkedList<T>::Direction::CLOCKWISE) {
+            newNode->prev = current;
+            (current->next)->prev = newNode;
+            newNode->next = current->next;
+            current->next = newNode;
+        } else {
+            newNode->next = current;
+            newNode->prev =current->prev;
+            current->prev = newNode;
+            (newNode->prev)->next = newNode;
+        }
+        current = newNode;
     }
+    length += 1;
+}
+
+template<typename T>
+int CircularDoublyLinkedList<T>::pull() {
+    if (len() > 0) {
+        int indexToReturn = current->index;
+
+        // перебрасываем связи, но current, все еще хранит инфу о соседях
+        (current->prev)->next = current->next;
+        (current->next)->prev = current->prev;
+
+        //запоминаем местонахождение следующего current в зависимости от направления
+        Node<T>* currentNextLoc = current->next;
+        if (dir == Direction::COUNTERCLOCKWISE)
+            currentNextLoc = current->prev;
+
+        delete current;
+
+        current = currentNextLoc;
+
+        length -= 1;
+        return indexToReturn;
+    }
+    return -1;
 }
 
 // go_next меняет узел current на следующий за ним
 template<typename T>
 void CircularDoublyLinkedList<T>::go_next() {
     if (current) {
-        current = current->next;
+        if (dir == Direction::CLOCKWISE) {
+            current = current->next;
+        } else {
+            current = current->prev;
+        }
     }
 }
 
@@ -91,6 +144,11 @@ void CircularDoublyLinkedList<T>::go_next() {
 template<typename T>
 Node<T> CircularDoublyLinkedList<T>::get_node() {
     return *current;
+}
+
+template<typename T>
+int CircularDoublyLinkedList<T>::len() {
+    return this->length;
 }
 
 #endif //CIRCULARDOUBLYLINKEDLIST_H
--- src/main.cpp
+++ src/main.cpp
@@ -4,14 +4,22 @@
 
 int main() {
     CircularDoublyLinkedList<int> list;
+    list.set_clockwise_direction();
     list.push_next(5);
     list.push_next(6);
     list.push_next(7);
     list.push_next(11);
     list.push_next(13);
+    list.set_counterclockwise_direction();
+    list.pull();
+    list.pull();
+    list.pull();
+    list.pull();
+    list.pull();
+    list.pull();
 
-    for (int i = 0; i < 3; ++i) {
-        std::cout << list.get_node().data;
+    for (int i = 0; i < list.len(); ++i) {
+        std::cout << list.get_node().data << std::endl;
         list.go_next();
     }
 
